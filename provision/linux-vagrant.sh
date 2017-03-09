0b66865464efea9d4c613f6b87cd74de5423f8ef
AJ <aj@laatu.uk>
2017-03-09 13:07:18 +0000

    fix for vagrant home dir owner

--- provision/linux-vagrant.sh
+++ provision/linux-vagrant.sh
@@ -10,6 +10,6 @@ echo "vagrant:vagrant" | chpasswd
 mkdir -p /home/vagrant/.ssh
 curl https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub > /home/vagrant/.ssh/authorized_keys
 
-chown -R vagrant:vagrant /home/vagrant/.ssh
+chown -R vagrant:vagrant /home/vagrant
 chmod 0700 /home/vagrant/.ssh
 chmod 0600 /home/vagrant/.ssh/authorized_keys
